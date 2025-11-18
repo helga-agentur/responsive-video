ac3fc21179ebe20d1c57870f0c6c52458ce42a0e
Töbu <tobias@helga.ch>
2025-11-18 18:37:28 +0100

    chore(): think...

--- src/EventSubscriber/ResponsiveVideoSubscriber.php
+++ src/EventSubscriber/ResponsiveVideoSubscriber.php
@@ -19,18 +19,27 @@ final class ResponsiveVideoSubscriber implements EventSubscriberInterface {
    * Kernel request event handler.
    */
   public function onVideoCreate(RequestEvent $event): void {
-    // @todo Place your code here.
+    /*
+     * 0. Check if Filesystem (/files/responsive_videos/YYYY-MM) is created
+     * 0.1 Create directory if not present
+     * 1. Save Video in /files/responsive_videos/YYYY-MM
+     * 2. get all possible Video Formats
+     * 3. get all responsive-video-styles
+     * 4. get all video-styles activated in responsive-video-styles
+     * 5. send all permutations of combinations to converter
+     * 6. check if filesystem is ready. Every video is saved in /files/responsive_videos/styles/{sylename}/YYYY-MM
+     */
   }
 
   /**
    * Kernel response event handler.
    */
   public function onVideoUpdate(ResponseEvent $event): void {
-    // @todo Place your code here.
+    // -> onVideoCreate
   }
 
   public function onVideoDelete(RequestEvent $event): void {
-    // @todo do something
+    // delete all assets and the original video
   }
 
   /**

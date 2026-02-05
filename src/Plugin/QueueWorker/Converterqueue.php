ab4f40afd31a26c7d5669472d8cff6ef1dcdf6e3
Stefanos Petrakis <stefanos@helga.ch>
2026-02-05 13:32:58 +0100

    feat: enable cron-integration
    although this documentation says otherwise:
    
    https://www.drupal.org/node/3344220
    
    I tested and the loaded plugin definition comes with cron = null
    
    which then causes the worker plugin to be excluded from normal crons runs

--- src/Plugin/QueueWorker/Converterqueue.php
+++ src/Plugin/QueueWorker/Converterqueue.php
@@ -16,7 +16,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 #[QueueWorker(
   id: 'responsive_video_converterqueue',
-  title: new TranslatableMarkup('ConverterQueue'),
+  title: new TranslatableMarkup('ConverterQueue'),  
+  cron: ['time' => 300]  
 )]
 final class Converterqueue extends QueueWorkerBase implements ContainerFactoryPluginInterface {
 
